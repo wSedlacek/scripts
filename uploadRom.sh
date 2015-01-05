f9861151ef9960b24c2a1c78c3c6d2093b4555a7
William Casey SEdlacek <w@sedlaceks.org>
2015-01-05 11:58:36 -0800

    Update v1.0.1

--- uploadRom.sh
+++ uploadRom.sh
@@ -7,9 +7,9 @@ cd $REPO_DIR
 echo -e '\0033\0143'
 clear
 
-mkdir -p $REPODIR/$DEVICE
-cp "$TARGET_DIR""$FILE" "$REPODIR"/"$DEVICE"/"$FILE"
-cp "$TARGET_DIR""$MD5" "$REPODIR"/"$DEVICE"/"$MD5"
+mkdir -p $REPO_DIR/$DEVICE
+cp "$TARGET_DIR""$FILE" "$REPO_DIR"/"$DEVICE"/"$FILE"
+cp "$TARGET_DIR""$MD5" "$REPO_DIR"/"$DEVICE"/"$MD5"
 
 #Drive
 if [ "$DRIVE" == "1" ]; then
@@ -20,8 +20,8 @@ if [ "$DRIVE" == "1" ]; then
 
     #Setup
     mkdir -p ~/Google\ Drive/Nightlies/$TODAY/$DEVICE
-    cp "$REPODIR"/"$DEVICE"/"$FILE" ~/Google\ Drive/Nightlies/$DATE/$DEVICE/$FILE
-    cp "$REPODIR"/"$DEVICE"/"$MD%" ~/Google\ Drive/Nightlies/$DATE/$DEVICE/$MD5
+    cp "$REPO_DIR"/"$DEVICE"/"$FILE" ~/Google\ Drive/Nightlies/$DATE/$DEVICE/$FILE
+    cp "$REPO_DIR"/"$DEVICE"/"$MD%" ~/Google\ Drive/Nightlies/$DATE/$DEVICE/$MD5
 
     #Upload
     grive -p ~/Google\ Drive &
@@ -35,19 +35,19 @@ fi
 if [[ "$FTPYN" == "1" ]]; then(
 . ftp-config
 echo "Uploading the following"
-echo "$ROMNAME"_"$DEVICE""$DATE"".zip"
-echo "$ROMNAME"_"$DEVICE""$DATE"".md5"
+echo "$ROM_NAME"_"$DEVICE""$DATE"".zip"
+echo "$ROM_NAME"_"$DEVICE""$DATE"".md5"
 echo "From"
-echo $REPODIR/$DEVICE
-cd $REPODIR/$DEVICE
-md5sum "$ROMNAME"_"$DEVICE""$DATE".zip > "$ROMNAME"_"$DEVICE""$DATE".zip.md5
+echo $REPO_DIR/$DEVICE
+cd $REPO_DIR/$DEVICE
+md5sum "$ROM_NAME"_"$DEVICE""$DATE".zip > "$ROM_NAME"_"$DEVICE""$DATE".zip.md5
 # login to ftp server and transfer file
 ftp -n -i $SERVER <<EOF
 user $USERNAME $PASSWORD
 cd $FTPHOMEDIR$FTPTARGETDIR
 binary
-mput "$ROMNAME"_"$DEVICE""$DATE".zip.md5
-mput "$ROMNAME"_"$DEVICE""$DATE".zip
+mput "$ROM_NAME"_"$DEVICE""$DATE".zip.md5
+mput "$ROM_NAME"_"$DEVICE""$DATE".zip
 quit
 EOF
 )fi
@@ -56,15 +56,15 @@ EOF
 if [[ "$SCPYN" == "1" ]]; then(
 . scp-config
 echo "Uploading the following"
-echo "$ROMNAME"_"$DEVICE""$DATE"".zip"
-echo "$ROMNAME"_"$DEVICE""$DATE"".md5"
+echo "$ROM_NAME"_"$DEVICE""$DATE"".zip"
+echo "$ROM_NAME"_"$DEVICE""$DATE"".md5"
 echo "From"
-echo $REPODIR/$DEVICE
-cd $REPODIR/$DEVICE
-md5sum "$ROMNAME"_"$DEVICE""$DATE".zip > "$ROMNAME"_"$DEVICE""$DATE".zip.md5
+echo $REPO_DIR/$DEVICE
+cd $REPO_DIR/$DEVICE
+md5sum "$ROM_NAME"_"$DEVICE""$DATE".zip > "$ROM_NAME"_"$DEVICE""$DATE".zip.md5
 # login to via scp and transfer file
-scp $REPODIR/$DEVICE/"$ROMNAME"_"$DEVICE""$DATE".zip.md5 $USERNAME@$SERVER:$FTPHOMEDIR/$ROMNAME/"$ROMNAME"_"$DEVICE""$DATE".zip.md5
+scp $REPO_DIR/$DEVICE/"$ROM_NAME"_"$DEVICE""$DATE".zip.md5 $USERNAME@$SERVER:$FTPHOMEDIR/$ROM_NAME/"$ROM_NAME"_"$DEVICE""$DATE".zip.md5
 echo $SCPPASSWORD
-scp $REPODIR/$DEVICE/"$ROMNAME"_"$DEVICE""$DATE".zip $USERNAME@$SERVER:$FTPHOMEDIR/$ROMNAME/"$ROMNAME"_"$DEVICE""$DATE".zip
+scp $REPO_DIR/$DEVICE/"$ROM_NAME"_"$DEVICE""$DATE".zip $USERNAME@$SERVER:$FTPHOMEDIR/$ROM_NAME/"$ROM_NAME"_"$DEVICE""$DATE".zip
 echo $SCPPASSWORD
 )fi
